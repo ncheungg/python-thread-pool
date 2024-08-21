9e140473ebab722ec640575f6cd8f34834925890
Nathan Cheung <10472191+ncheungg@users.noreply.github.com>
2024-08-20 22:48:23 -0700

    added shutdown method, with cancelled tasks

--- thread_pool.py
+++ thread_pool.py
@@ -9,12 +9,17 @@ _INCOMPLETE = object()
 
 class Task:
     def __init__(
-        self, fn: Callable[..., Any], args: List[Any], callback: Callable[[], None]
+        self,
+        fn: Callable[..., Any],
+        args: List[Any],
+        callback: Callable[[], None],
+        cancelled: bool = False,
     ) -> None:
         self.result_val: Any = _INCOMPLETE
         self.finished_event = Event()
         self.wrapped_fn = self._get_wrapped_fn(fn, args)
         self._callback = callback
+        self._is_cancalled = cancelled
 
     def _get_wrapped_fn(self, fn: Callable[..., Any], args: List[Any]) -> None:
         def wrapper():
@@ -28,12 +33,18 @@ class Task:
         thread = Thread(target=self.wrapped_fn)
         thread.start()
 
+    def cancelled(self) -> bool:
+        return self._is_cancalled
+
     def finished(self) -> bool:
         return self.result_val is not _INCOMPLETE
 
     def result(self) -> Any:
         if self.finished():
             return self.result_val
+
+        if self.cancelled():
+            return None
 
         self.finished_event.wait()
         return self.result_val
@@ -44,6 +55,7 @@ class ThreadPool:
         self.workers = workers
         self.tasks: Deque[Task] = deque()
         self.lock = Lock()
+        self.is_shutdown = False
 
     def _task_done(self) -> None:
         with self.lock:
@@ -54,6 +66,9 @@ class ThreadPool:
                 self.workers += 1
 
     def submit(self, fn: Callable[..., Any], args: List[Any] | None = None) -> Task:
+        if self.is_shutdown:
+            return Task(fn, args, self._task_done, cancelled=True)
+
         task = Task(fn, args, self._task_done)
 
         with self.lock:
@@ -64,3 +79,6 @@ class ThreadPool:
                 self.tasks.append(task)
 
         return task
+
+    def shutdown(self) -> None:
+        self.is_shutdown = True
